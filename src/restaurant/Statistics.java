12dd2b35cb2b7200ba2502fa1e8383a58659dc77
Alfons Lorente Darder <alorente@cifpfbmoll.com>
2021-11-09 17:47:28 +0100

    Init Commit

---
+++ src/restaurant/Statistics.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurant;
+
+import javax.swing.JTable;
+
+/**
+ *
+ * @author alfon
+ */
+public class Statistics extends JTable{
+        
+}
---
+++ src/restaurant/Viewer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurant;
+
+import java.awt.Canvas;
+
+/**
+ *
+ * @author alfon
+ */
+public class Viewer extends Canvas  {
+    
+}
