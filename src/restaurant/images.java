1a34d92eae08171d9b87450730beb43f4bbafe5b
Alfons Lorente <alorente@cifpfbmoll.eu>
2021-11-15 13:24:28 +0100

    Commit2

---
+++ src/restaurant/images.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurant;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author alfon
+ */
+public class images {
+    Hashtable<String, BufferedImage> imgs = new Hashtable<>();
+    
+    public images(){
+        setImages();
+    }
+    
+    public BufferedImage loadImage(String name){
+        BufferedImage img = imgs.get(name);
+        return img;
+    }
+    
+    private void setImages(){
+        try {
+            this.imgs.put("cookingchef", ImageIO.read(new File("IMG/cookingchef.png")));
+            this.imgs.put("standbychef", ImageIO.read(new File("IMG/standbychef.png")));
+            this.imgs.put("eatingclient", ImageIO.read(new File("IMG/eatingclient.png")));
+            this.imgs.put("standbyclient", ImageIO.read(new File("IMG/standbyclient.png")));
+            this.imgs.put("table", ImageIO.read(new File("IMG/table.png")));
+            
+        } catch (IOException ex) {
+            Logger.getLogger(images.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
+
+
